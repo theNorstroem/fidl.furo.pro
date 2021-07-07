e93fa686cf4074ddba116b01b570eb478e18bed6
veith <veithz@gmail.com>
2021-07-07 17:05:53 +0200

    moved to eclipsefuro

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 # remove old search indexes
 rm docs/en.search*
 rm -rf tmp
-git clone --depth 1 git@github.com:eclipse/eclipsefuro.git ./tmp
+git clone --depth 1 --sparse git@github.com:eclipse/eclipsefuro.git ./tmp
 
 # build docs
-hugo -s ./tmp/docs/furo -d ../../../docs
+hugo -s ./tmp/docs/fidl -d ../../../docs
 rm -rf tmp
 
 # add changes
--- readme.md
+++ readme.md
@@ -1,7 +1,3 @@
 # furo FIDL docs
 
-This is the repo for the documentation itself. 
-
-Feel free to fill an issue or pull request to improove the documentation.
-
-Maybe you are more interested in the [spectools](https://github.com/theNorstroem/spectools).
+run build.sh to update the documentation

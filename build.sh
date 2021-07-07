545768d8d96f5f7e3c6bb76da92c5c7bdf4273da
veith <veithz@gmail.com>
2021-07-07 17:03:48 +0200

    autocommit publish docs

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+
 # remove old search indexes
 rm docs/en.search*
+rm -rf tmp
+git clone --depth 1 git@github.com:eclipse/eclipsefuro.git ./tmp
+
 # build docs
-hugo -d ./docs
+hugo -s ./tmp/docs/furo -d ../../../docs
+rm -rf tmp
+
 # add changes
 git add docs/.
+
 # autocommit
 git commit -m "autocommit publish docs"
 # publish by pushing changes
